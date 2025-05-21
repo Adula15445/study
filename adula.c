12db41f3c7cab4dba0367b81e41bc1dea7f6a392
adula <whitenora807@gmail.com>
2025-05-21 19:29:57 +0900

    C commit

---
+++ adula.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    // char op = 0;
+    // int num1 = 0, num2 = 0;
+    // printf("연산자 :");
+    // scanf("%c", &op);
+    // printf("정수 2개 입력 :");
+    // scanf("%d %d", &num1, &num2);
+
+    // if (op == '+'){
+    //     printf("%d + %d = %d\n", num1, num2, num1 + num2);
+    // }
+    int i = 0;
+    while (i < 10)
+    {
+        printf("이야 매우 성공적!!\n");
+        i++;
+    }
+    
+    return 0;
+}
